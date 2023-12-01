a8fac7a343011b580e1b4ab7c108d586e4a39213
Peter Jones <peternjones99@gmail.com>
2023-12-01 17:48:44 -0500

    LAST COMMIT BEFORE DELETIONS

---
+++ Computational Notes.R
@@ -0,0 +1,9 @@
+
+
+## Research Question
+
+# Does level of press freedom have a differential impact on the success of self-determination movements
+# relative to the year at which their country widely adopted internet usage?
+
+# In other words, might the potential for internet usage to aid self-determination movement success
+# be less salient under central governments that suppress their media/civil society spheres?
