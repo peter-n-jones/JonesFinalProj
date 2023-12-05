4bdb129b793e1502d0b05c25bee42f64c5710190
Peter Jones <peternjones99@gmail.com>
2023-12-05 12:51:48 -0500

    less than free

--- Computational Notes.R
+++ Computational Notes.R
@@ -7,3 +7,14 @@
 
 # In other words, might the potential for internet usage to aid self-determination movement success
 # be less salient under central governments that suppress their media/civil society spheres?
+
+
+
+
+
+
+#to do
+
+- organize code
+- fix some data (eg Latvia, NAs for missing names)
+- fix col names etc
